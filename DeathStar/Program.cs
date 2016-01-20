70d99b22b077a88ce3c0a9cd912d64f6f6aca011
John Fletcher <jbfletcher47@hotmail.com>
2016-01-20 09:45:46 -0600

    added titanic function to check for ice

--- DeathStar/Program.cs
+++ DeathStar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopSecretPlans;
 
 
 namespace DeathStar
@@ -11,6 +12,8 @@ namespace DeathStar
     {
         static void Main(string[] args)
         {
+           
+
         }
     }
 }
---
+++ DeathStar/Titanic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathStar
+{
+    class Titanic
+    {
+        public int numberOfLookouts = 2;
+        public int numberOfLookoutPosts = 2;
+        public int numberOfIcebergs;
+        public int iceDamage = 0;
+
+
+
+        public Titanic(int numberOfLookouts, int numberOfLookoutPosts)
+        {
+            this.numberOfLookouts = numberOfLookouts;
+            this.numberOfLookoutPosts = numberOfLookoutPosts;
+            
+        }
+        public void lookoutOnDuty()
+        {
+            Console.WriteLine("Warrant Officer! Lookout Status!");
+            Console.WriteLine("Ice watch One and Two on duty Sir!");
+
+        } 
+
+
+        public int checkForIcebergs(int iceDamage, int numberOfIcebergs)
+        {
+            this.numberOfIcebergs = numberOfIcebergs;
+            this.iceDamage = iceDamage;
+            Random iceberg = new Random();
+            int bergCheck = iceberg.Next(iceDamage);
+            return bergCheck;
+            if(bergCheck <= 1)
+            {
+                Console.WriteLine("No ice in sight!. Full Speed ahead, Sir!");
+            }
+            else
+            {
+                Console.WriteLine("ICE dead ahead sir!");
+            }
+
+        }
+    }
+
+}
+
